57868a6856ead137ed173fd12cd200ba9dbea6c5
jdogcodey <jonnydawson@hotmail.com>
2025-03-21 15:14:37 +0000

    chore: add a few things for the app to use

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ import passport from "passport";
 import "dotenv/config";
 import routes from "./routes/indexRouter";
 import "./config/passport";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
