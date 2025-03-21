027d07e4245d158ecfab44e96849e694430cbb00
jdogcodey <jonnydawson@hotmail.com>
2025-03-21 15:44:31 +0000

    feat: discovered some new parameters

--- app.js
+++ app.js
@@ -11,5 +11,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({ extended: true, limit: "1mb", parameterLimit: 5000 })
+);
 app.use(express.static(path.join(__dirname, "public")));
