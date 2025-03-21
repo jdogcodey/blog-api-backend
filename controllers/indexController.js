00287a273deec519e5a8d0da3c39c622cb2b73fc
jdogcodey <jonnydawson@hotmail.com>
2025-03-21 16:42:34 +0000

    feat: add a basic homepage controller

---
+++ controllers/indexController.js
@@ -0,0 +1,11 @@
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import { body, validationResult, check } from "express-validator";
+import "dotenv";
+import prisma from "../config/prisma-client.js";
+
+const controller = {
+  homepage: (req, res, next) => {
+    res.json({ message: "all good!" });
+  },
+};
